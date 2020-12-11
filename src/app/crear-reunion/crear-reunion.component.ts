599611a3571628fc423b51ea72c2b64e5d196c99
AfricaSimonSesmero <55829240+AfricaSimonSesmero@users.noreply.github.com>
2020-12-11 19:00:14 +0100

    Add files via upload

--- src/app/crear-reunion/crear-reunion.component.ts
+++ src/app/crear-reunion/crear-reunion.component.ts
@@ -12,16 +12,11 @@ import { UsuarioService } from '../services/usuario.service';
 })
 export class CrearReunionComponent implements OnInit {
   constructor(public router: Router, private reunionServicio: ReunionService, private usuarioServicio: UsuarioService) { }
-  temas: string;
-  descripcion: string;
+  tituloConvocar: string;
+  descripcionConvocar: string;
   horaFin: string;
-  horaInicio: string;
-  asistentes: string[] = [];
-  convocante: string;
-  respuesta: ReunionDto;
-  nombreUsuario = localStorage.getItem("name");
-  usuariosRegistrados: UsuarioDto[];
-  usuariosElegidos: UsuarioDto[] = [];
+  fechaconvocar: string;
+  asistentesConvocar: string[] = []
   indexAdd: number = null;
   indexDelete: number = null;
 
@@ -30,7 +25,7 @@ export class CrearReunionComponent implements OnInit {
   ngOnInit(): void{
         this.usuarioServicio.getAll().subscribe({
       next: (usuariosReceived: UsuarioDto[]) =>{
-        (this.usuariosRegistrados = usuariosReceived);
+        (this.asistentesConvocar = asistentesConvocar);
       },
       error: (err) => {
         console.error(err);
@@ -41,43 +36,23 @@ export class CrearReunionComponent implements OnInit {
 
 
   reunion(): void {
-    console.log(this.asistentes);
 
-    const reunion: ReunionDto = {
-      temas: this.temas,
-      descripcion: this.descripcion,
-      horaFin: this.horaFin,
-      horaInicio: this.horaInicio,
-      asistentes: this.asistentes,
-      convocante: this.nombreUsuario
-    };
 
-    this.reunionServicio
-    .crear_reunion(reunion)
-    .subscribe({
-      next: (resp: ReunionDto) => {
-        this.respuesta = resp;
-      },
-      error: (err) => {
-        console.error(err);
-      },
-      complete: () => (console.log("OK")),
-    });
   }
 
   deleteAsistente(): void{
     if (this.indexDelete != null){
-      this.usuariosRegistrados.push(this.usuariosElegidos[this.indexDelete]);
-      this.asistentes.splice(this.indexDelete, 1);
-      this.usuariosElegidos.splice(this.indexDelete, 1);
+      this.asistentesConvocar.push(this.asistentesConvocar[this.indexDelete]);
+      this.asistentesConvocar.splice(this.indexDelete, 1);
+      this.asistentesConvocar.splice(this.indexDelete, 1);
     }
   }
 
   addAsistente(): void{
     if (this.indexAdd != null){
-      this.usuariosElegidos.push(this.usuariosRegistrados[this.indexAdd]);
-      this.asistentes.push(this.usuariosRegistrados[this.indexAdd].username);
-      this.usuariosRegistrados.splice(this.indexAdd, 1);
+      this.asistentesConvocar.push(this.asistentesConvocar[this.indexAdd]);
+      this.asistentesConvocar.push(this.asistentesConvocar[this.indexAdd].username);
+      this.asistentesConvocar.splice(this.indexAdd, 1);
 
     }
   }
